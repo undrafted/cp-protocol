e521d13256847e45cfdb75f567a4ea9bda54e5bc
Jermie Domingo <8jermiedomingo@gmail.com>
2025-06-07 12:11:01 +0200

    minor refactor

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,50 @@
 use num_bigint::{BigUint, RandBigInt};
 use rand;
 
-// r1 == alpha^s * y1^c
-// r2 == beta^s * y2^c
-pub fn verify(
-    r1: &BigUint,
-    r2: &BigUint,
-    alpha: &BigUint,
-    beta: &BigUint,
-    y1: &BigUint,
-    y2: &BigUint,
-    c: &BigUint,
-    s: &BigUint,
-    q: &BigUint,
-) -> bool {
-    let lhs1 = (alpha.modpow(s, q) * y1.modpow(c, q)) % q;
-    let lhs2 = (beta.modpow(s, q) * y2.modpow(c, q)) % q;
-    lhs1 == *r1 && lhs2 == *r2
+pub struct Proof {
+    pub p: BigUint,     // prime modulus
+    pub q: BigUint,     // order of the group
+    pub alpha: BigUint, // generator of the group
+    pub beta: BigUint,  // generator of the group
 }
 
-// s = k - c * x
-// k cant be less then c * x
-pub fn solve(k: &BigUint, c: &BigUint, x: &BigUint, q: &BigUint) -> BigUint {
-    if *k >= c * x {
-        return (k - c * x).modpow(&BigUint::from(1u32), q);
+impl Proof {
+    // r1 == alpha^s * y1^c
+    // r2 == beta^s * y2^c
+    pub fn verify(
+        &self,
+        r1: &BigUint,
+        r2: &BigUint,
+        y1: &BigUint,
+        y2: &BigUint,
+        c: &BigUint,
+        s: &BigUint,
+    ) -> bool {
+        let lhs1 = (&self.alpha.modpow(s, &self.p) * y1.modpow(c, &self.p))
+            .modpow(&BigUint::from(1u32), &self.p);
+        let lhs2 = (&self.beta.modpow(s, &self.p) * y2.modpow(c, &self.p))
+            .modpow(&BigUint::from(1u32), &self.p);
+
+        lhs1 == *r1 && lhs2 == *r2
     }
 
-    return q - (c * x - k).modpow(&BigUint::from(1u32), q);
-}
+    // s = k - c * x
+    // k cant be less then c * x
+    pub fn solve(&self, k: &BigUint, c: &BigUint, x: &BigUint) -> BigUint {
+        if *k >= c * x {
+            return (k - c * x).modpow(&BigUint::from(1u32), &self.q);
+        }
 
-// n^exp mod p
-pub fn exponentiate(n: &BigUint, exp: &BigUint, p: &BigUint) -> BigUint {
-    n.modpow(exp, p)
-}
+        return &self.q - (c * x - k).modpow(&BigUint::from(1u32), &self.q);
+    }
 
-pub fn generate_random_less_than(limit: &BigUint) -> BigUint {
-    let mut rng = rand::thread_rng();
-    rng.gen_biguint_below(limit)
+    // n^exp mod p
+    pub fn exponentiate(n: &BigUint, exp: &BigUint, p: &BigUint) -> BigUint {
+        n.modpow(exp, p)
+    }
+
+    pub fn generate_random_less_than(limit: &BigUint) -> BigUint {
+        let mut rng = rand::thread_rng();
+        rng.gen_biguint_below(limit)
+    }
 }
--- tests/lib_test.rs
+++ tests/lib_test.rs
@@ -1,4 +1,4 @@
-use cp_protocol::*;
+use cp_protocol::Proof;
 use num_bigint::BigUint;
 
 #[test]
@@ -8,33 +8,40 @@ fn test() {
     let p = BigUint::from(23u32);
     let q = BigUint::from(11u32);
 
+    let proof = Proof {
+        p: p.clone(),
+        q: q.clone(),
+        alpha: alpha.clone(),
+        beta: beta.clone(),
+    };
+
     let x = BigUint::from(6u32);
     let k = BigUint::from(7u32);
 
     let c = BigUint::from(4u32);
 
-    let y1 = exponentiate(&alpha, &x, &p);
-    let y2 = exponentiate(&beta, &x, &p);
+    let y1 = Proof::exponentiate(&alpha, &x, &p);
+    let y2 = Proof::exponentiate(&beta, &x, &p);
 
     assert_eq!(y1, BigUint::from(2u32));
     assert_eq!(y2, BigUint::from(3u32));
 
-    let r1 = exponentiate(&alpha, &k, &p);
-    let r2 = exponentiate(&beta, &k, &p);
+    let r1 = Proof::exponentiate(&alpha, &k, &p);
+    let r2 = Proof::exponentiate(&beta, &k, &p);
 
     assert_eq!(r1, BigUint::from(8u32));
     assert_eq!(r2, BigUint::from(4u32));
 
-    let s = solve(&k, &c, &x, &q);
+    let s = proof.solve(&k, &c, &x);
     assert_eq!(s, BigUint::from(5u32));
 
-    let result = verify(&r1, &r2, &alpha, &beta, &y1, &y2, &c, &s, &p);
+    let result = proof.verify(&r1, &r2, &y1, &y2, &c, &s);
     assert!(result);
 
     let x_fake = BigUint::from(7u32);
-    let s = solve(&k, &c, &x_fake, &q);
+    let s = proof.solve(&k, &c, &x_fake);
 
-    let result = verify(&r1, &r2, &alpha, &beta, &y1, &y2, &c, &s, &p);
+    let result = proof.verify(&r1, &r2, &y1, &y2, &c, &s);
     assert!(!result);
 }
 
@@ -45,22 +52,29 @@ fn test_rand() {
     let p = BigUint::from(23u32);
     let q = BigUint::from(11u32);
 
+    let proof = Proof {
+        p: p.clone(),
+        q: q.clone(),
+        alpha: alpha.clone(),
+        beta: beta.clone(),
+    };
+
     let x = BigUint::from(6u32);
-    let k = generate_random_less_than(&q);
+    let k = Proof::generate_random_less_than(&q);
 
-    let c = generate_random_less_than(&q);
+    let c = Proof::generate_random_less_than(&q);
 
-    let y1 = exponentiate(&alpha, &x, &p);
-    let y2 = exponentiate(&beta, &x, &p);
+    let y1 = Proof::exponentiate(&alpha, &x, &p);
+    let y2 = Proof::exponentiate(&beta, &x, &p);
 
     assert_eq!(y1, BigUint::from(2u32));
     assert_eq!(y2, BigUint::from(3u32));
 
-    let r1 = exponentiate(&alpha, &k, &p);
-    let r2 = exponentiate(&beta, &k, &p);
+    let r1 = Proof::exponentiate(&alpha, &k, &p);
+    let r2 = Proof::exponentiate(&beta, &k, &p);
 
-    let s = solve(&k, &c, &x, &q);
+    let s = proof.solve(&k, &c, &x);
 
-    let result = verify(&r1, &r2, &alpha, &beta, &y1, &y2, &c, &s, &p);
+    let result = proof.verify(&r1, &r2, &y1, &y2, &c, &s);
     assert!(result);
 }
