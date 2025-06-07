57405cc7caca85a20cf3179bf40d2c13e9351663
Jermie Domingo <8jermiedomingo@gmail.com>
2025-06-07 15:01:17 +0200

    tonic

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ fn main() {
     tonic_build::configure()
         .build_server(true)
         .out_dir("src/") // you can change the generated code's location
-        .compile_protos(
+        .compile(
             &["proto/auth.proto"],
             &["proto/"], // specify the root location to search proto dependencies
         )
